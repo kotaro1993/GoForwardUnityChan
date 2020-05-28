2250b6b3ae4866ab5ecd5928ce0afe239bea10b3
kotaro1993 <kotaro.s.1993@gmail.com>
2020-05-28 21:59:26 +0900

    GoForwardUnityChan 初回コミット
    初回コミットです

---
+++ Assets/CubeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CubeController : MonoBehaviour {
+
+    public AudioClip sound1;
+    AudioSource audioSource;
+    private float speed = -12;
+    private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+        
+	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       if(other.gameObject.tag =="Cube" || other.gameObject.tag == "Ground")
+        {
+            audioSource.PlayOneShot(sound1);
+        }
+    }
+}
